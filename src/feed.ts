17859728e15a714f5cc23b9c1e672416d5477b80
infinitY0369 <flutist.sud.0v@icloud.com>
2023-10-24 16:23:27 +0900

    Add HTML parser

--- src/feed.ts
+++ src/feed.ts
@@ -1,9 +1,13 @@
 import { XMLParser } from "fast-xml-parser"
+import { parse } from "node-html-parser"
 
 export function* parseFeedContent(url: string) {
     const reg = {
         github: {
             commits: /https:\/\/github.com\/.*\/.*\/commits\/.*\.atom/
+        },
+        monsterhunter: {
+            news: /https:\/\/www.monsterhunter.com\/j?a?\/?news\/?/
         }
     }
 
@@ -20,10 +24,11 @@ export function* parseFeedContent(url: string) {
     }
 
     const parser = new XMLParser(parserOptions)
-    const jobj = parser.parse(xml)
 
     switch (true) {
-        case reg.github.commits.test(url):
+        case reg.github.commits.test(url): {
+            const jobj = parser.parse(xml)
+
             for (const entry of jobj.feed.entry.reverse()) {
                 yield {
                     title: entry.title,
@@ -33,6 +38,20 @@ export function* parseFeedContent(url: string) {
                 }
             }
             break
+        }
+        case reg.monsterhunter.news.test(url): {
+            const jobj = parse(xml)
+
+            for (const item of jobj.querySelectorAll("#latest > div > ul > a").reverse()) {
+                yield {
+                    title: item.querySelector("li > figure > figcaption > div.text")?.innerText.trim(),
+                    link: item.getAttribute("href"),
+                    author: item.querySelector("li > figure > figcaption > div.info > div.category")?.innerText,
+                    updated: item.querySelector("li > figure > figcaption > div.info > div.date")?.innerText.trim()
+                }
+            }
+            break
+        }
         default:
             Logger.log("No parser is defined for this URL.")
             break
